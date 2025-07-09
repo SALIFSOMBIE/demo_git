b92335f3f1a5e989f81f9ed8c94568caccb654e4
Liya S. Hailu <liyashimeles2372@gmail.com>
2025-07-09 11:09:03 +0300

    I have added a bash scrip for argument examples

---
+++ arguments.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+echo "Script Name: $0"
+echo "First Argument: $1"
+echo "Second Argument: $2"
+echo "Other Arguments: $3,$4, ..."
+echo "All Arguments: $@"
+echo "Total Arguments: $#"
